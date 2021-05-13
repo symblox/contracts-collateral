5f359d360fe9c4a0d8480659afe09da39a91dd18
Symblox Dev <symblox@protonmail.com>
2021-05-13 19:40:08 +0800

    chg: change ISYXSwap interface.

--- contracts/interfaces/synth/ISYXSwap.sol
+++ contracts/interfaces/synth/ISYXSwap.sol
@@ -2,5 +2,16 @@
 pragma solidity >=0.4.24;
 
 interface ISYXSwap {
+    /**
+     * @dev get buy token address e.g syx
+     */
+    function targetToken() external view returns (address);
+
+    /**
+     * @dev buy syx
+     *
+     * @param token sell token address
+     * @param amount sell amount
+     */
     function buySyx(address token, uint256 amount) external payable;
 }
